f2afcd4147d05204c97eaae52c3c01369774a2a8
Samson Orr <171129@student.vansd.org>
2022-12-09 20:53:36 +0000

    Style changes

--- index.php
+++ index.php
@@ -9,14 +9,22 @@ session_start();
   <meta name="viewport" content="width=device-width">
   <title>File Upload</title>
   <link href="style.css" rel="stylesheet" type="text/css" />
+  <link rel="icon" type="image/x-icon" href="/uploads/favicon.ico">
 </head>
 <body>
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
-  Select file to upload:
+  Select file to upload (Files above 500KB may or may not upload):
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload File" name="submit">
 </form>
-
+<div class=footer>
+<?php
+echo "Date at server location: " . date("l");
+echo ", " . date("m/d/Y") . "<br>";
+?>
+&copy; <?php echo date("Y");?>
+<?php echo " | Anything you upload here is public."?>
+</div>
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -10,12 +10,6 @@ if (file_exists($target_file)) {
   $uploadOk = 0;
 }
 
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
   echo "Sorry, your file was not uploaded.";
