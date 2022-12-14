33438586c6b6bf3ad4618f0ad6691ce4ba29356e
Samson Orr <171129@student.vansd.org>
2022-12-14 21:53:59 +0000

    Initial commit

--- index.php
+++ index.php
@@ -8,23 +8,29 @@ session_start();
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
   <title>File Upload</title>
-  <link href="style.css" rel="stylesheet" type="text/css" />
+  
+  <link href="https://cdn.jsdelivr.net/npm/modern-normalize/modern-normalize.min.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">  
+<link href="style.css" rel="stylesheet" type="text/css" />
   <link rel="icon" type="image/x-icon" href="/uploads/favicon.ico">
+<script src="https://www.w3schools.com/lib/w3.js"></script>
 </head>
 <body>
-
+<div class="wrapper">
+<button onclick="window.location.href='https://tehost.ml';" class="button" class="w3-bar-item w3-button">Home</button>
+<button onclick="window.location.href='/upload.php';" class="button" class="w3-bar-item w3-button">File Upload</button>
+<button onclick="history.back()" class="button" class="w3-bar-item w3-button">Go Back</button>
+</div>
+<div class="wrapper">
 <form action="upload.php" method="post" enctype="multipart/form-data">
   Select file to upload (Files above 500KB may or may not upload):
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload File" name="submit">
+  <input type="file" name="fileToUpload" id="fileToUpload" class="button">
+  <input type="submit" value="Upload File" name="submit" class="button">
 </form>
-<div class=footer>
-<?php
-echo "Date at server location: " . date("l");
-echo ", " . date("m/d/Y") . "<br>";
-?>
-&copy; <?php echo date("Y");?>
-<?php echo " | Anything you upload here is public."?>
+</div>
+<div class="wrapper">
+<?php echo "Anything you upload here is public."?>
+<button onclick="window.location.href='https://legal.tehost.ml';" class="button">See terms</button>
 </div>
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -1,3 +1,15 @@
+<link rel="icon" type="image/x-icon" href="/uploads/favicon.ico">
+
+<link href="https://cdn.jsdelivr.net/npm/modern-normalize/modern-normalize.min.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link href="style.css" rel="stylesheet" type="text/css" />
+<script src="https://www.w3schools.com/lib/w3.js"></script>
+<div class="wrapper">
+<button onclick="window.location.href='https://tehost.ml';" class="button" class="w3-bar-item w3-button">Home</button>
+<button onclick="window.location.href='/index.php';" class="button" class="w3-bar-item w3-button">File Upload</button>
+<button onclick="history.back()" class="button" class="w3-bar-item w3-button">Go Back</button>
+</div>
+<div class="wrapper">
 <?php
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
@@ -12,7 +24,7 @@ if (file_exists($target_file)) {
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
+  echo " Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
@@ -22,3 +34,4 @@ if ($uploadOk == 0) {
   }
 }
 ?>
+</div>
